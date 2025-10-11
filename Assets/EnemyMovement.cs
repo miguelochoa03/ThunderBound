05174be7624968e3f1b0d7fffa7874e7e346fe5c
MIGUEL <anthonymiguelochoa03@gmail.com>
2025-10-11 16:51:09 -0700

    Init

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public GameObject player;
+
+    public bool flip;
+
+    public float speed;
+
+    public float jumpPower;
+    public bool isJumping;
+
+    public float jumpCooldown;
+
+    public float health = 20f;
+    public GameObject bloodEffect;
+
+    private float dazedTime;
+    public float startDazedTime;
+
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // die when health reaches zero
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        // handles dazing the enemy
+        if (dazedTime <= 0)
+        {
+            speed = 5;
+        } else
+        {
+            speed = 0;
+            dazedTime -= Time.deltaTime;
+        }
+
+            Vector3 scale = transform.localScale;
+
+        if (player.transform.position.x > transform.position.x)
+        {
+            scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 1);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        } else
+        {
+            scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1);
+            transform.Translate(speed * Time.deltaTime * -1, 0, 0);
+        }
+
+        transform.localScale = scale;
+
+        if (Time.time > jumpCooldown && !isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isJumping = true;
+            jumpCooldown = Time.time + 3;
+        }
+        
+
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        // want to add knockback
+
+        // will daze the enemy when hit
+        dazedTime = startDazedTime;
+        // play a hurt sound
+
+        // blood
+        Instantiate(bloodEffect, transform.position, Quaternion.identity);
+        health -= damage;
+        Debug.Log("Damage TAKEN !");
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float horizontalInput;
+    float moveSpeed = 5f;
+    bool isFacingLeft = false;
+    float jumpPower = 4f;
+    bool isJumping = false;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        FlipSprite();
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isJumping = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+    }
+    void FlipSprite()
+    {
+        if (isFacingLeft && horizontalInput > 0f || !isFacingLeft && horizontalInput < 0f)
+        {
+            isFacingLeft = !isFacingLeft;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
+    }
+}
